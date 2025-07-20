8f6e1f1737d61473e73a1e3646afbcd4230823c9
Mehdi ZAYANI <msz.dev91@gmail.com>
2025-07-20 17:27:05 +0200

    feat(cli): add professional stylish welcome screen

---
+++ src/ui/cliWelcome.mjs
@@ -0,0 +1,17 @@
+import chalk from 'chalk';
+import figlet from 'figlet';
+import gradient from 'gradient-string';
+
+export function displayWelcome() {
+  console.clear();
+
+  const msg = figlet.textSync('Create Nexter', {
+    font: 'Standard',
+    horizontalLayout: 'default',
+    verticalLayout: 'default'
+  });
+
+  console.log(gradient.pastel.multiline(msg));
+  console.log(chalk.cyanBright('Welcome to create-nexter CLI!\n'));
+console.log(chalk.white('Effortless Next.js project scaffolding, built for scalable and maintainable applications.\n'));
+}
